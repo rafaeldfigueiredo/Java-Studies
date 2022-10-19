109e0e2903f3f0a9afaa4dc3144dee6dec7a70c6
Rafael 'Fig' Diamantino de Figueiredo <45023203+rafaeldfigueiredo@users.noreply.github.com>
2022-10-19 12:39:11 -0300

    fixes

--- Guest List/addGuest.py
+++
@@ -1,4 +0,0 @@
-f = open("guests.txt","a")
-guest = input('Tell me a name:\n>')
-f.write(f"{guest}\n")
-f.close()
--- Guest List/addGuestList.py
+++
@@ -1,6 +0,0 @@
-import json
-
-potentialGuests = open("./potentialGuests.json","r")
-guestList = open("guests.txt","r")
-
-json.dump(potentialGuests, guestList)
