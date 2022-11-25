bab18d30ff4a769056480956e62aed972164ca4d
Rafael 'Fig' Diamantino de Figueiredo <45023203+rafaeldfigueiredo@users.noreply.github.com>
2022-11-25 22:58:05 +0000

    add files

---
+++ Main.java
@@ -0,0 +1,31 @@
+class Main {
+  /*
+   * Você foi convidado(a) a participar de um projeto que visa estudar as reações
+   * de pessoas isoladas em um bioma.
+   * Sua função será registrar as informações geradas por três grupos de sensores,
+   * dentro de um ambiente controlado.
+   * Para isso, considere que os sensores estão espalhados por todos os espaços do
+   * ambiente e buscam registrar a temperatura(int temp), a umidade(int umi) e a quantidade de gás
+   * carbônico(int coTwo). Assim, conforme as informações citadas, você deverá:
+   * 
+   * — criar rotinas que registrem os dados enviados por esses sensores em cada
+   * espaço do ambiente
+   * — criar estruturas de dados que servirão para armazenar o valor apresentado
+   * por cada sensor e o momento da leitura (hora)(int[][] tempReg)(int[][] umiReg)(int[][] coTwo)
+   * 
+   * Importante:
+   * — Para direcionar você na elaboração da sua atividade, destaco que:
+   * você deve usar matriz para implementar essas estruturas, criando ao menos uma
+   * classe específica para cada estrutura;
+   * 
+   * — Após cada registro armazenado, deve ser utilizado um ou mais algoritmo(s) de
+   * ordenação para apresentar os dados classificados, decrescente para a umidade
+   * e crescentemente para temperatura e o CO₂, mostrando no fim da rotina dos
+   * dois;
+   *
+   * — As matrizes ou matriz deve(m) ter, no máximo, 48 linhas e os registros devem
+   * ser feitos a cada meia hora do dia.
+   */
+  
+
+}
---
+++ rotinas.java
@@ -0,0 +1,7 @@
+public class rotinas {
+  
+
+  public static void main(String[] args) {
+  
+ } 
+}
